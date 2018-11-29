adceb82aba8f674d681477f700e568d96a59c997
Taufiq Abrory <abroryrb7@gmail.com>
2018-11-29 23:38:48 +0800

    fix salah migrate tabel users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username');
+            $table->string('nama');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('jeniskelamin');
+            $table->text('alamat');
+            $table->text('foto');
+            $table->text('bio');
+            $table->integer('reputasi');
+            $table->integer('userlevel_id')->unsigned();
+            $table->foreign('userlevel_id')->references('id')->on('userlevel');
             $table->rememberToken();
             $table->timestamps();
         });
